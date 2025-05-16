1d564e3b6aa560162f770fffaeaf5d593934d49a
nasifayc <nasifayc11@gmail.com>
2025-05-16 14:07:35 +0300

    feat: database setup, request dto interface and data validation added

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
 
 // import { User, Bookmark } from '@prisma/client';
 
-@Injectable({})
+@Injectable()
 export class AuthService {
+  constructor(private prisma: PrismaService) {}
   login() {
     return { message: 'I am new signed in' };
   }
-  signup() {
-    return { message: 'I am new signed up' };
+  async signup(dto: AuthDto) {
+    // generate the password
+    const hash = await argon.hash(dto.password);
+
+    // save the new user
+    const user = await this.prisma.user.create({
+      data: {
+        email: dto.email,
+        hash,
+      },
+    });
+
+    // return the saved user
+
+    const { hash: _, ...userWithoutHash } = user;
+    return userWithoutHash;
   }
 }
